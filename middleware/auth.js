dcfb859213590108d87958dab85e94bf2160ee7a
jonathan_nsiah <jooankrah@gmail.com>
2021-06-18 16:20:17 +0000

    no message

--- middleware/auth.js
+++ middleware/auth.js
@@ -4,8 +4,6 @@ const User = require("../models/user");
 const auth = async (req, res, next) => {
   try {
     const token = req.headers.authorization.split(" ")[1];
-    console.log(req.headers.authorization);
-
     const decoded = jwt.verify(token, "GodistheGreatest");
     const user = await User.findOne({
       _id: decoded._id,
